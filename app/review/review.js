b0510f19e8614162b146abce9a9ac16f9021c2d7
Machinewarlock <jameschu1874@gmail.com>
2016-08-20 19:24:54 +0100

    Averages now adding up to get a total property score

--- app/review/review.js
+++ app/review/review.js
@@ -3,30 +3,19 @@ $('.prop').hide();
 var username = getUsername();
 $('.username').html(username);
 
+var average = 0
+var average2 = 0
+var average3 = 0
+var average4 = 0
+var average5 = 0
+var average6 = 0
+var average7 = 0
+var average8 = 0
+var average9 = 0
+var average10 = 0
+
 function processSubmit(){
-$("#form1").submit();
-$("#form2").submit();
-$("#form3").submit();
-$("#form4").submit();
-$("#form5").submit();
-$("#form6").submit();
-$("#form7").submit();
-$("#form8").submit();
-$("#form9").submit();
-$("#form10").submit();
 
-var rating = $("#form1").submit();
-var rating2 = $("#form2").submit();
-var rating3 = $("#form3").submit();
-var rating4 = $("#form4").submit();
-var rating5 = $("#form5").submit();
-var rating6 = $("#form6").submit();
-var rating7 = $("#form7").submit();
-var rating8 = $("#form8").submit();
-var rating9 = $("#form9").submit();
-var rating10 = $("#form10").submit();
-
-	  if (rating = true){
 		var a = parseInt($("input[name=rating]:checked").val());
 			database.ref('Livingroom').update({
 	  		[username]: a
@@ -40,15 +29,14 @@ var rating10 = $("#form10").submit();
 	 			userCount += 1;
 	 			ratingSum += data.val();
 	 		});
-	 		var average = ratingSum / userCount;
+	 		average = ratingSum / userCount;
 			database.ref('Livingroom-average').update({
 	  		Score: average
 	 	});	 		
 	 		$(".results").html(userCount + " votes recorded (" + average.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 
-	};
-	  if (rating2 = true){
+
 		var b = parseInt($("input[name=ratingtwo]:checked").val());
 			database.ref('Diningroom').update({
 	  		[username]: b
@@ -62,15 +50,14 @@ var rating10 = $("#form10").submit();
 	 			userCount2 += 1;
 	 			ratingSum2 += data.val();
 	 		});
-	 		var average2 = ratingSum2 / userCount2;
+	 		average2 = ratingSum2 / userCount2;
 			database.ref('Diningroom-average').update({
 	  		Score: average2
 	 	});	 		
 	 		$(".results2").html(userCount2 + " votes recorded (" + average2.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 
-	}
-	  if (rating3 = true){
+
 		var c = parseInt($("input[name=ratingthree]:checked").val());
 			database.ref('Bedroom').update({
 	  		[username]: c
@@ -84,15 +71,14 @@ var rating10 = $("#form10").submit();
 	 			userCount3 += 1;
 	 			ratingSum3 += data.val();
 	 		});
-	 		var average3 = ratingSum3 / userCount3;
+	 		average3 = ratingSum3 / userCount3;
 			database.ref('Bedroom-average').update({
 	  		Score: average3
 	 	});	 		
 	 		$(".results3").html(userCount3 + " votes recorded (" + average3.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating4 = true){
+
 		var d = parseInt($("input[name=ratingfour]:checked").val());
 			database.ref('Bathroom').update({
 	  		[username]: d
@@ -106,15 +92,14 @@ var rating10 = $("#form10").submit();
 	 			userCount4 += 1;
 	 			ratingSum4 += data.val();
 	 		});
-	 		var average4 = ratingSum4 / userCount4;
+	 		average4 = ratingSum4 / userCount4;
 			database.ref('Bathroom-average').update({
 	  		Score: average4
 	 	});	 		
 	 		$(".results4").html(userCount4 + " votes recorded (" + average4.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating5 = true){
+
 		var e = parseInt($("input[name=ratingfive]:checked").val());
 			database.ref('Garden').update({
 	  		[username]: e
@@ -128,15 +113,14 @@ var rating10 = $("#form10").submit();
 	 			userCount5 += 1;
 	 			ratingSum5 += data.val();
 	 		});
-	 		var average5 = ratingSum5 / userCount5;
+	 		average5 = ratingSum5 / userCount5;
 			database.ref('Garden-average').update({
 	  		Score: average5
 	 	});	 		
 	 		$(".results5").html(userCount5 + " votes recorded (" + average5.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating6 = true){
+
 		var f = parseInt($("input[name=ratingsix]:checked").val());
 			database.ref('Room-Sizes').update({
 	  		[username]: f
@@ -150,15 +134,14 @@ var rating10 = $("#form10").submit();
 	 			userCount6 += 1;
 	 			ratingSum6 += data.val();
 	 		});
-	 		var average6 = ratingSum6 / userCount6;
+	 		average6 = ratingSum6 / userCount6;
 			database.ref('Room-Sizes-average').update({
 	  		Score: average6
 	 	});	 			 		
 	 		$(".results6").html(userCount6 + " votes recorded (" + average6.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating7 = true){
+
 		var g = parseInt($("input[name=ratingseven]:checked").val());
 			database.ref('Lighting').update({
 	  		[username]: g
@@ -172,15 +155,14 @@ var rating10 = $("#form10").submit();
 	 			userCount7 += 1;
 	 			ratingSum7 += data.val();
 	 		});
-	 		var average7 = ratingSum7 / userCount7;
+	 		average7 = ratingSum7 / userCount7;
 			database.ref('Lighting-average').update({
 	  		Score: average7
 	 	});	 		
 	 		$(".results7").html(userCount7 + " votes recorded (" + average7.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating8 = true){
+
 		var h = parseInt($("input[name=ratingeight]:checked").val());
 			database.ref('Condition').update({
 	  		[username]: h
@@ -194,15 +176,14 @@ var rating10 = $("#form10").submit();
 	 			userCount8 += 1;
 	 			ratingSum8 += data.val();
 	 		});
-	 		var average8 = ratingSum8 / userCount8;
+	 		average8 = ratingSum8 / userCount8;
 			database.ref('Condition-average').update({
 	  		Score: average8
 	 	});	 		
 	 		$(".results8").html(userCount8 + " votes recorded (" + average8.toFixed(1) + " rating)");
 	 	}, function(mlh){});
 			
-	}
-	  if (rating9 = true){
+
 		var i = parseInt($("input[name=ratingnine]:checked").val());
 			database.ref('Price').update({
 	  		[username]: i
@@ -216,15 +197,14 @@ var rating10 = $("#form10").submit();
 	 			userCount9 += 1;
 	 			ratingSum9 += data.val();
 	 		});
-	 		var average9 = ratingSum9 / userCount9;
+	 		average9 = ratingSum9 / userCount9;
 	 		database.ref('Price-average').update({
 	  		Score: average9
 	 	});	 		
 	 		$(".results9").html(userCount9 + " votes recorded (" + average9.toFixed(1) + " rating)");
 	 	}, function(mlh){});
-			
-	}
-	  if (rating10 = true){
+
+
 		var j = parseInt($("input[name=ratingten]:checked").val());
 			database.ref('Location').update({
 	  		[username]: j
@@ -238,32 +218,18 @@ var rating10 = $("#form10").submit();
 	 			userCount10 += 1;
 	 			ratingSum10 += data.val();
 	 		});
-	 		var average10 = ratingSum10 / userCount10;
+	 		average10 = ratingSum10 / userCount10;
 			database.ref('Location-average').update({
 	  		Score: average10
 	 	});	 		
 	 		$(".results10").html(userCount10 + " votes recorded (" + average10.toFixed(1) + " rating)");
+	 		var total = (average + average2 + average3 + average4 + average5 + average6 + average7 + average8 + average9 + average10).toFixed(1);
+			database.ref('Property-Score').update({
+			Score: total
+		});	
+			$('.prop').show();
+			$('#avg').html("Total Property Score " + total);
 	 	}, function(mlh){});
-			
-	}
-	var avg = 1
-	var avg2 = 2
-	var avg3 = 3
-	var avg4 = 4
-	var avg5 = 5
-	var avg6 = 1
-	var avg7 = 2
-	var avg8 = 4
-	var avg9 = 5
-	var avg10 = 1 
-	var total = avg + avg2 + avg3 + avg4 + avg5 + avg6 + avg7 + avg8 + avg9 + avg10;
-	
-		database.ref('Property-Score').update({
-		Score: total
-	});
-
-	$('.prop').show();
-	$('#avg').html("Total Property Score " + total);
-	// $(location).attr('href', '/app/thank-you/')   
+	//$(location).attr('href', '/app/thank-you/')   
 	return false;
 };
